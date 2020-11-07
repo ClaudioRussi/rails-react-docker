43c18252af2f05510ab4c884bb16253085a80ef9
Claudio Russi <claudiomrussi@hotmail.com>
2020-11-07 16:39:43 -0300

    Fix docker-compose db and add user and card models

---
+++ be/db/migrate/20201107190240_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cards do |t|
+      t.string :name
+      t.text :description
+      t.string :image_url
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
